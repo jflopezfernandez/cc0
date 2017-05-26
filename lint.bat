ab19fd8344a8a7e06839b7f8edb2d5d4836d2605
JFLF <jflopezfernandez@gmail.com>
2017-05-26 08:34:07 -0400

    Can now successfully calculate number gcd of whole numbers

---
+++ lint.bat
@@ -0,0 +1,2 @@
+@echo off
+cppcheck . 2> err.txt
--- main.c
+++ main.c
@@ -15,10 +15,13 @@ struct _fraction {
 	int d;
 };
 
+void printFraction(struct _fraction *f);
+
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END FRACTION
-enum _symbolic { PI, e };
+enum _symbolic { PI, e, x, y };
 
 double getValueOfSymbolicConstant(enum _symbolic symbol);
+void printSymbolicConstant(enum _symbolic symbol);
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END SYMBOLIC
 
@@ -31,10 +34,17 @@ union _component {
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END COMPONENT
 
+enum _numberType { FRACTION, WHOLE, DECIMAL, SYMBOLIC };
+
 struct _number {
 	union _component real;
+	enum _numberType realType;
+	
 	union _component complex;
+	enum _numberType complexType;
 };
+
+void printNumber(struct _number *n);
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END NUMBER
 
@@ -50,24 +60,77 @@ void freeMatrix(struct _matrix *m);
 void matrix(int n);
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ ENDMATRIX
+
+/** TODO: Implement equation as a linked list? */
+
+struct _number getGCD(struct _number *n1, struct _number *n2);
+
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ *****************
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ * START PROGRAM *
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ *****************
 
 int main()
 {
+	struct _number n1;
+	n1.realType = WHOLE;
+	n1.real.whole = 1071;
 	
+	struct _number n2;
+	n2.realType = WHOLE;
+	n2.real.whole = 462;
+	
+	struct _number gcd = getGCD(&n1,&n2);
+	
+	printf("gcd(%i,%i)=%i\n",n1.real.whole,n2.real.whole,gcd.real.whole);
 	
 	return EXIT_SUCCESS;
 }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END PROGRAM 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END PROGRAM
+
+
+void printFraction(struct _fraction *f) {
+	if (f->n == 0) {
+		printf("0 ");
+	} else if (f->d == 1) {
+		printf("%i ", f->n);
+	} else {
+		printf("%i/%i ", f->n, f->d);
+	}
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END FRACTION
 
 double getValueOfSymbolicConstant(enum _symbolic symbol) {
+	if (symbol == PI) {
+		return ((double) 22 / (double) 7);
+	} else if (symbol == e) {
+		return (2.72);
+	}
+	
 	return 0.0;
 }
 
+
+void printSymbolicConstant(enum _symbolic symbol) {
+	if (symbol == PI) {
+		printf("pi");
+	} else if (symbol == e) {
+		printf("e");
+	} else if (symbol == x) {
+		printf("x");
+	} else if (symbol == y) {
+		printf("y");
+	} else {
+		printf("[unknown symbolic constant]");
+	}
+}
+
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END SYMBOLIC
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END COMPONENT
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END NUMBER
 
 struct _matrix* createMatrix() {
 	struct _matrix *newMatrix = malloc(sizeof(struct _matrix));
@@ -107,3 +170,31 @@ void matrix(int n) {
 }
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ END MATRIX
+
+
+struct _number getGCD(struct _number *n1, struct _number *n2) {
+	struct _number gcd;
+	gcd.real.whole = -1;
+	
+	if ((n1->realType != WHOLE) && (n2->realType != WHOLE)) {
+		printf("Cannot compute GCD for non-integral types!\n");
+		
+		return gcd;
+	}
+
+	int m = n1->real.whole;
+	int n = n2->real.whole;
+	int r = m % n;
+	
+	while (r != 0) {
+		m = n;
+		n = r;
+		
+		r = m % n;
+	}
+	
+	gcd.realType = WHOLE;
+	gcd.real.whole = n;
+	
+	return gcd;
+}
