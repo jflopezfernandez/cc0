1bd76aba83323e518b54e1460fb9e879d19872db
JFLF <jflopezfernandez@gmail.com>
2017-05-28 10:33:55 -0400

    Putting mpa on hold while I figure this out. Working on matrix manipulation for now.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 
 #include "includes\Main.h"
 
-#define MAX_PRECISION 12000
-char aSample[MAX_PRECISION+1];
-
-int cAdd(char *aOne, char *aTwo, char *aThree);
-
 
 int main()
 {
@@ -16,20 +11,4 @@ int main()
 	
 	
 	return EXIT_SUCCESS;
-}
-
-
-int cAdd(char *aOne, char *aTwo, char *aThree) {
-	int iStatus = 0;
-	int carry = 0;
-	int j = 0;
-	
-	for (int i = MAX_PRECISION - 1; i >= 0; i--) {
-		j = *(aOne+i) + *(aTwo+i) + carry - '0' - '0';
-		carry = j / 10;
-		j = j % 10;
-		*(aThree+i) = j + '0';
-	}
-	
-	return iStatus;
 }
