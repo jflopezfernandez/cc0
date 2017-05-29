676df1aaec572347b6866445934b0eb440860dcd
JFLF <jflopezfernandez@gmail.com>
2017-05-29 13:15:51 -0400

    Working on numerical data structure

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 
 #include "includes\Main.h"
 
+
+#define DIMENSION 10
+#define RANGE 10
+
+
+struct _number {
+	int num;
+	int den;
+};
+
+void printNumber(struct _number *n);
+
+struct _matrix {
+	struct _number m[DIMENSION][DIMENSION];
+};
+
+
+void setZeroMatrix(struct _matrix *m);
+void setIdentityMatrix(struct _matrix *m);
+void setRandMatrix(struct _matrix *m);
 void printMatrix(struct _matrix *m);
 
 
@@ -9,31 +29,74 @@ int main()
 	initializeProgram();
 	/** Start Program */
 	
-	struct _matrix m;
-	int count = 0;
-	int j = 0;
-	
-	for (int i = 0; i < DIMENSION; ++i) {
-		for (int j = 0; j < DIMENSION; ++j) {
-			m.m[i][j] = ++count;
-		}
-	}
-	
-	
+	struct _matrix matrix;
+	setRandMatrix(&matrix);
+	printMatrix(&matrix);
 	
 	
 	return EXIT_SUCCESS;
 }
 
 
+void printNumber(struct _number *n) {
+	if (n->den == 0) {
+		printf("  NaN ");
+	} else {
+		if (n->num == 0) {
+			printf("%5i ", n->num);
+		} else if (n->den == 1) {
+			printf("%5i ", n->num);
+		} else {
+			printf("%2i/%2i ", n->num, n->den);
+		}
+	}
+}
+
+
+void setZeroMatrix(struct _matrix *m) {
+	for (int i = 0; i < DIMENSION; ++i) {
+		for (int j = 0; j < DIMENSION; ++j) {
+			//m->m[i][j].num = 0;
+		}
+	}
+}
+
+
+void setIdentityMatrix(struct _matrix *m) {
+	for (int i = 0; i < DIMENSION; ++i) {
+		for (int j = 0; j < DIMENSION; ++j) {
+			if (i == j) {
+				m->m[i][j].num = 1;
+				m->m[i][j].den = 1;
+			} else {
+				m->m[i][j].num = 0;
+				m->m[i][j].den = 1;
+			}
+		}
+	}
+}
+
+
+void setRandMatrix(struct _matrix *m) {
+	const int range = RANGE;
+	srand(13);
+	
+	for (int i = 0; i < DIMENSION; ++i) {
+		for (int j = 0; j < DIMENSION; ++j) {
+			m->m[i][j].num = (rand() % range);
+			m->m[i][j].den = (rand() % range);
+		}
+	}
+}
+
+
 void printMatrix(struct _matrix *m) {
 	for (int i = 0; i < DIMENSION; ++i) {
 		for (int j = 0; j < DIMENSION; ++j) {
-			#if FLP
-				printf("%f ", m->m[i][j]);
-			#else
-				printf("%i ", m->m[i][j]);
-			#endif
+			printNumber(&(m->m[i][j]));
+			printf(" | ");
 		}
+		
+		printf("\n\n");
 	}
 }
